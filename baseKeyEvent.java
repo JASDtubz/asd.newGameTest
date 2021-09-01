434ca900dfb4f877cd3564eb31f2be6244337f62
JASDtubz <86506578+JASDtubz@users.noreply.github.com>
2021-08-31 21:28:52 -0500

    Added walls and better avatar

--- baseKeyEvent.java
+++ baseKeyEvent.java
@@ -17,8 +17,9 @@ public class HelloApplication extends Application
     static int[] realPlayer = { 0, 0 };
     static int[] realScreen = { 0, 0 };
     static int road = 500;
+    static int[][] walls = {{ -1000, -1000 }, { -950, -1000 }, { 1000, -1000 }, { -950, 1000 }};
 
-    static String text;
+    static String text = "";
     boolean blue = false;
     boolean info = false;
     boolean fullscreen = false;
@@ -59,95 +60,98 @@ public class HelloApplication extends Application
 
     public void update(KeyCode kc)
     {
-        switch (kc)
-        {
-            case W:
-                if (player[1] != 0)
-                    playerMove(false, -50);
-                break;
-            case A:
-                if (player[0] != 0)
-                    playerMove(true, -50);
-                break;
-            case S:
-                if (player[1] != 550)
-                    playerMove(false, 50);
-                break;
-            case D:
-                if (player[0] != 850)
-                    playerMove(true, 50);
-                break;
-            case UP:
-                if (player[1] != 550)
-                    everything(false, 50);
-                break;
-            case LEFT:
-                if (player[0] != 850)
-                    everything(true, 50);
-                break;
-            case DOWN:
-                if (player[1] != 0)
-                    everything(false, -50);
-                break;
-            case RIGHT:
-                if (player[0] != 0)
-                    everything(true, -50);
-                break;
-            case Z:
-                if ((player[0] == box[0] && player[1] == box[1] + 50 && playerHead == 0) ||
-                    (player[0] == box[0] - 50 && player[1] == box[1] && playerHead == 1) ||
-                    (player[0] == box[0] && player[1] == box[1] - 50 && playerHead == 2) ||
-                    (player[0] == box[0] + 50 && player[1] == box[1] && playerHead == 3))
-                        text = "An unmovable box.";
-                else if ((player[0] == portal[0] && player[1] == portal[1] + 50 && playerHead == 0) ||
-                    (player[0] == portal[0] - 50 && player[1] == portal[1] && playerHead == 1) ||
-                    (player[0] == portal[0] && player[1] == portal[1] - 50 && playerHead == 2) ||
-                    (player[0] == portal[0] + 50 && player[1] == portal[1] && playerHead == 3))
-                        text = "A mysterious portal.";
-                else if (player[0] == portal[0] && player[1] == portal[1])
-                    this.blue = true;
-                break;
-            case F:
-                if (!this.fullscreen)
-                {
-                    this.stage.setMaximized(true);
-                    this.fullscreen = true;
-                }
-                else
-                {
-                    this.stage.setMaximized(false);
-                    this.fullscreen = false;
-                    this.bp.setLeft(new VBox());
-                    this.info = false;
-                }
-                break;
-            case ENTER:
-                if (!this.info && this.fullscreen)
-                {
-                    this.bp.setLeft(this.vb);
-                    this.info = true;
-                }
-                else if (this.info && this.fullscreen)
-                {
-                    this.bp.setLeft(new VBox());
-                    this.info = false;
-                }
-                break;
-            case SPACE:
-                realPlayer[0] = 0;
-                realPlayer[1] = 0;
-                realScreen[0] = 0;
-                realScreen[1] = 0;
-                playerHead = 0;
-                player[0] = 400;
-                player[1] = 300;
-                portal[0] = 200;
-                portal[1] = 200;
-                box[0] = -500;
-                box[1] = -100;
-                road = 500;
-                this.blue = false;
-        }
+        if (!text.equals("") && kc == KeyCode.E)
+            text = "";
+        else if (text.equals(""))
+            switch (kc)
+            {
+                case W:
+                    if (player[1] != 50)
+                        this.playerMove(false, -50);
+                    else
+                        this.everything(false, 50);
+                    break;
+                case A:
+                    if (player[0] != 50)
+                        this.playerMove(true, -50);
+                    else
+                        this.everything(true, 50);
+                    break;
+                case S:
+                    if (player[1] != 500)
+                        this.playerMove(false, 50);
+                    else
+                        this.everything(false, -50);
+                    break;
+                case D:
+                    if (player[0] != 800)
+                        this.playerMove(true, 50);
+                    else
+                        this.everything(true, -50);
+                    break;
+                case E:
+                    if ((player[0] == box[0] && player[1] == box[1] + 50 && playerHead == 0) ||
+                        (player[0] == box[0] - 50 && player[1] == box[1] && playerHead == 1) ||
+                        (player[0] == box[0] && player[1] == box[1] - 50 && playerHead == 2) ||
+                        (player[0] == box[0] + 50 && player[1] == box[1] && playerHead == 3))
+                            text = "An unmovable box.";
+                    else if ((player[0] == portal[0] && player[1] == portal[1] + 50 && playerHead == 0) ||
+                        (player[0] == portal[0] - 50 && player[1] == portal[1] && playerHead == 1) ||
+                        (player[0] == portal[0] && player[1] == portal[1] - 50 && playerHead == 2) ||
+                        (player[0] == portal[0] + 50 && player[1] == portal[1] && playerHead == 3))
+                            text = "A mysterious portal.";
+                    else if (player[0] == portal[0] && player[1] == portal[1])
+                        this.blue = true;
+                    break;
+                case F:
+                    if (!this.fullscreen)
+                    {
+                        this.stage.setMaximized(true);
+                        this.fullscreen = true;
+                    }
+                    else
+                    {
+                        this.stage.setMaximized(false);
+                        this.fullscreen = false;
+                        this.bp.setLeft(new VBox());
+                        this.info = false;
+                    }
+                    break;
+                case ENTER:
+                    if (!this.info && this.fullscreen)
+                    {
+                        this.bp.setLeft(this.vb);
+                        this.info = true;
+                    }
+                    else if (this.info && this.fullscreen)
+                    {
+                        this.bp.setLeft(new VBox());
+                        this.info = false;
+                    }
+                    break;
+                case R:
+                    realPlayer[0] = 0;
+                    realPlayer[1] = 0;
+                    realScreen[0] = 0;
+                    realScreen[1] = 0;
+                    playerHead = 0;
+                    player[0] = 400;
+                    player[1] = 300;
+                    portal[0] = 200;
+                    portal[1] = 200;
+                    box[0] = -500;
+                    box[1] = -100;
+                    road = 500;
+                    walls[0][0] = -1000;
+                    walls[0][1] = -1000;
+                    walls[1][0] = -950;
+                    walls[1][1] = -1000;
+                    walls[2][0] = 1000;
+                    walls[2][1] = -1000;
+                    walls[3][0] = -950;
+                    walls[3][1] = 1000;
+                    this.blue = false;
+            }
 
         this.redraw();
     }
@@ -167,52 +171,55 @@ public class HelloApplication extends Application
             this.greenRoad(realScreen[0] % 2 == 0);
 
         this.gc.setFill(Color.RED);
-        this.gc.fillRect(player[0], player[1], 50, 50);
-        this.gc.setFill(Color.WHITE);
-
-        double[] x = new double[3];
-        double[] y = new double[3];
 
         switch (playerHead)
         {
             case 0:
-                x[0] = player[0] + 25;
-                x[1] = player[0] + 12;
-                x[2] = player[0] + 37;
-                y[0] = player[1];
-                y[1] = player[1] + 12;
-                y[2] = player[1] + 12;
+                this.gc.fillRect(player[0], player[1] + 10, 50, 40);
+                this.gc.setFill(Color.WHITE);
+                this.gc.fillRect(player[0], player[1], 10, 10);
+                this.gc.fillRect(player[0] + 40, player[1], 10, 10);
+                this.gc.setFill(Color.YELLOW);
+                this.gc.fillRect(player[0] + 20, player[1] + 10, 10, 10);
+                this.gc.fillRect(player[0] + 10, player[1] + 20, 30, 20);
                 break;
             case 1:
-                x[0] = player[0] + 50;
-                x[1] = player[0] + 37;
-                x[2] = player[0] + 37;
-                y[0] = player[1] + 25;
-                y[1] = player[1] + 12;
-                y[2] = player[1] + 37;
+                this.gc.fillRect(player[0], player[1], 40, 50);
+                this.gc.setFill(Color.WHITE);
+                this.gc.fillRect(player[0] + 40, player[1], 10, 10);
+                this.gc.fillRect(player[0] + 40, player[1] + 40, 10, 10);
+                this.gc.setFill(Color.YELLOW);
+                this.gc.fillRect(player[0] + 30, player[1] + 20, 10, 10);
+                this.gc.fillRect(player[0] + 10, player[1] + 10, 20, 30);
                 break;
             case 2:
-                x[0] = player[0] + 25;
-                x[1] = player[0] + 37;
-                x[2] = player[0] + 12;
-                y[0] = player[1] + 50;
-                y[1] = player[1] + 37;
-                y[2] = player[1] + 37;
+                this.gc.fillRect(player[0], player[1], 50, 40);
+                this.gc.setFill(Color.WHITE);
+                this.gc.fillRect(player[0] + 40, player[1] + 40, 10, 10);
+                this.gc.fillRect(player[0], player[1] + 40, 10, 10);
+                this.gc.setFill(Color.YELLOW);
+                this.gc.fillRect(player[0] + 20, player[1] + 30, 10, 10);
+                this.gc.fillRect(player[0] + 10, player[1] + 10, 30, 20);
                 break;
             case 3:
-                x[0] = player[0];
-                x[1] = player[0] + 12;
-                x[2] = player[0] + 12;
-                y[0] = player[1] + 25;
-                y[1] = player[1] + 37;
-                y[2] = player[1] + 12;
+                this.gc.fillRect(player[0] + 10, player[1], 40, 50);
+                this.gc.setFill(Color.WHITE);
+                this.gc.fillRect(player[0], player[1] + 40, 10, 10);
+                this.gc.fillRect(player[0], player[1], 10, 10);
+                this.gc.setFill(Color.YELLOW);
+                this.gc.fillRect(player[0] + 10, player[1] + 20, 10, 10);
+                this.gc.fillRect(player[0] + 20, player[1] + 10, 20, 30);
         }
 
-        this.gc.fillPolygon(x, y, 3);
         this.gc.setFill(Color.BLUE);
         this.gc.fillRect(portal[0], portal[1], 50, 50);
         this.gc.setFill(Color.DARKGREEN);
         this.gc.fillRect(box[0], box[1], 50, 50);
+        this.gc.setFill(Color.GRAY);
+        this.gc.fillRect(walls[0][0], walls[0][1], 50, 2050);
+        this.gc.fillRect(walls[1][0], walls[1][1], 1950, 50);
+        this.gc.fillRect(walls[2][0], walls[2][1], 50, 2050);
+        this.gc.fillRect(walls[3][0], walls[3][1], 1950, 50);
         this.gc.setFill(Color.BLACK);
         this.gc.fillRect(0, 600, 900, 50);
         this.gc.setFill(Color.WHITE);
@@ -223,8 +230,6 @@ public class HelloApplication extends Application
         this.screenShift.setText("Screen Position " + realScreen[0] + " " + realScreen[1]);
         this.screenCoords.setText("Screen Coordinates " + player[0] + " " + player[1]);
         this.playerState.setText("Player State " + playerHead);
-
-        text = "";
     }
 
     public void playerMove(boolean b, int i)
@@ -234,7 +239,8 @@ public class HelloApplication extends Application
             playerHead = i > 0 ? 1 : 3;
             player[0] += i;
 
-            if (player[0] == box[0] && player[1] == box[1])
+            if ((player[0] == box[0] && player[1] == box[1]) ||
+                (player[0] == walls[0][0] || player[0] == walls[2][0]))
             {
                 player[0] -= i;
                 return;
@@ -247,7 +253,8 @@ public class HelloApplication extends Application
             playerHead = i > 0 ? 2 : 0;
             player[1] += i;
 
-            if (player[0] == box[0] && player[1] == box[1])
+            if ((player[0] == box[0] && player[1] == box[1]) ||
+                (player[1] == walls[1][1] || player[1] == walls[3][1]))
             {
                 player[1] -= i;
                 return;
@@ -261,18 +268,47 @@ public class HelloApplication extends Application
     {
         if (b)
         {
-            realScreen[0] += i > 0 ? -1: 1;
-            player[0] += i;
+            playerHead = i > 0 ? 3 : 1;
+            realScreen[0] += i > 0 ? -1 : 1;
             box[0] += i;
             portal[0] += i;
+
+            for (int i_ = 0; i_ < walls.length; i_++)
+                walls[i_][0] += i;
+
+            if (player[0] == box[0] && player[1] == box[1] ||
+                (player[0] == walls[0][0] || player[0] == walls[2][0]))
+            {
+                realScreen[0] -= i > 0 ? -1 : 1;
+                box[0] -= i;
+                portal[0] -= i;
+
+                for (int i_ = 0; i_ < walls.length; i_++)
+                    walls[i_][0] -= i;
+            }
         }
         else
         {
+            playerHead = i > 0 ? 0 : 2;
             realScreen[1] += i > 0 ? 1 : -1;
-            player[1] += i;
             box[1] += i;
             portal[1] += i;
             road += i;
+
+            for (int i_ = 0; i_ < walls.length; i_++)
+                walls[i_][1] += i;
+
+            if (player[0] == box[0] && player[1] == box[1] ||
+                (player[1] == walls[1][1] || player[1] == walls[3][1]))
+            {
+                realScreen[1] -= i > 0 ? 1 : -1;
+                box[1] -= i;
+                portal[1] -= i;
+                road -= i;
+
+                for (int i_ = 0; i_ < walls.length; i_++)
+                    walls[i_][1] -= i;
+            }
         }
     }
 
@@ -290,7 +326,7 @@ public class HelloApplication extends Application
             this.gc.setFill(Color.GREEN);
         else
             this.gc.setFill(Color.LIME);
-            
+
         for (int i = 0; i < 900; i += 20)
             for (int j = road; j < road + 50; j += 20)
                 this.gc.fillRect(i, j, 10, 10);
@@ -303,11 +339,11 @@ public class HelloApplication extends Application
             this.gc.setFill(Color.LIME);
         else
             this.gc.setFill(Color.GREEN);
-            
+
         for (int i = 10; i < 900; i += 20)
             for (int j = road; j < road + 50; j += 20)
                 this.gc.fillRect(i, j, 10, 10);
-                
+
         for (int i = 0; i < 900; i += 20)
             for (int j = road + 10; j < road + 50; j += 20)
                 this.gc.fillRect(i, j, 10, 10);
