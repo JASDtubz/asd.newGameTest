f4a52d7cfbfe9fe63b98e8bad0a1e49479149ad5
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-02 22:12:54 -0500

    Update Main.java

--- mapCumSheet/Main.java
+++ mapCumSheet/Main.java
@@ -1,38 +1,12 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        File f = new File("C:\\Users\\asdje\\IdeaProjects\\dotCUMtest\\src\\thing.cum");
-        Scanner s = new Scanner(f);
-        StringBuilder sb = new StringBuilder();
-
-        while (s.hasNextLine())
-        {
-            String str = s.nextLine();
-            sb.append(str);
-            System.out.println(str);
-        }
-
-        System.out.println(sb);
-
-        boolean b = true;
-        while (b)
-        {
-            int i = 0;
-            try
-            {
-                i = sb.indexOf(" ");
-                sb.deleteCharAt(i);
-            }
-            catch (Exception ignored)
-            {
-                b = false;
-            }
-        }
-
-        System.out.println(sb);
+        Lotion l = new Lotion();
+        HashMap hm = l.execute();
+        
     }
 }
