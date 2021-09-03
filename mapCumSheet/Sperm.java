9bac95e5b4264c90b51eab1e3295226f5b98be05
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-02 22:15:21 -0500

    Create Sperm.java

---
+++ mapCumSheet/Sperm.java
@@ -0,0 +1,14 @@
+public class Sperm
+{
+    public String name;
+    public boolean collision;
+
+    public Sperm()
+    {
+    }
+
+    public void setName(String s)
+    {
+        this.name = s;
+    }
+}
