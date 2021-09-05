162cc9092681be40a268d7022949f9dad42f788d
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-04 22:13:33 -0500

    comes with an inner class

--- mapCumSheet/Sperm.java
+++ mapCumSheet/Sperm.java
@@ -1,14 +1,35 @@
+import java.util.ArrayList;
+
 public class Sperm
 {
     public String name;
     public boolean collision;
+    public ArrayList<Texture> texture = new ArrayList<>();
 
-    public Sperm()
+    public Sperm() { }
+
+    public void setName(String s) { this.name = s; }
+
+    public void setCollision(boolean b) { this.collision = b; }
+
+    public void setTexture(int r, int b, int g, int x, int y, int l, int h)
     {
+        texture.add(new Texture(r, g, b, x, y, l, h));
     }
+}
 
-    public void setName(String s)
+class Texture
+{
+    public int r, b, g, x, y, l, h;
+
+    public Texture(int r, int b, int g, int x, int y, int l, int h)
     {
-        this.name = s;
+        this.r = r;
+        this.b = b;
+        this.g = g;
+        this.x = x;
+        this.y = y;
+        this.l = l;
+        this.h = h;
     }
 }
