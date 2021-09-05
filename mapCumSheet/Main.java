c08b8863628261f0585555c617fa0d2f45a28292
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-04 22:13:09 -0500

    yea

--- mapCumSheet/Main.java
+++ mapCumSheet/Main.java
@@ -1,12 +1,13 @@
-import java.io.*;
 import java.util.*;
 
 public class Main
 {
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args)
     {
         Lotion l = new Lotion();
-        HashMap hm = l.execute();
-        
+        HashMap<Integer, Sperm> hm = l.execute();
+        System.out.println(hm.get(0).name);
+        System.out.println(hm.get(0).collision);
+
     }
 }
