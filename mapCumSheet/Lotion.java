b000e6de71bfada2cffe4317a20f8b00cfed6663
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-09 08:40:35 -0500

    should be fixed??

--- mapCumSheet/Lotion.java
+++ mapCumSheet/Lotion.java
@@ -14,33 +14,33 @@ public class Lotion
         int i = 0;
         int line = 0;
         
-        while (scan.hasNextLine())  //Scans cum file
+        while (scan.hasNextLine())
         {
             s = new Sperm();
             str = scan.nextLine();
             line++;
             
-            if (!str.equals("}"))   //scans until {
+            if (!str.equals("}"))
                 sb.append(str);
             else
             {
                 boolean bool = true;
                 
-                while (bool)    //clears whitespace
+                while (bool)
                     try { sb.deleteCharAt(sb.indexOf(" ")); }
                     catch (Exception ignored) { bool = false; }
 
-                try { sb.deleteCharAt(sb.indexOf("{")); }   //checks for syntax
+                try { sb.deleteCharAt(sb.indexOf("{")); }
                 catch (Exception ignored) { System.out.println("No open curly bracket found at " + line); }
 
-                try     //checks for syntax
+                try
                 {
                     sb.delete(0, sb.indexOf(":"));
                     bool = true;
                 }
                 catch (Exception ignored) { System.out.println("No name found at " + line); }
 
-                if (bool)   //sets name of map object
+                if (bool)
                 {
                     try { sb.deleteCharAt(sb.indexOf(":")); }
                     catch (Exception ignored) { System.out.println("No colon found at " + line); }
@@ -75,80 +75,83 @@ public class Lotion
                     bool = true;
                 }
                 catch (Exception ignored) { System.out.println("No state found at " + line); }
-
+                
                 if (bool)
                 {
-                    try { sb.delete(sb.indexOf(":"), sb.indexOf("[")); }
-                    catch (Exception ignored) { System.out.println("No colon or open bracket found at " + line); }
-
                     boolean b0 = true;
-
+                    
                     while (b0)
                     {
-                        int r = 0;
-                        int g = 0;
-                        int b = 0;
-                        int x = 0;
-                        int y = 0;
-                        int l = 0;
-                        int h = 0;
-
-                        try { sb.delete(0, sb.indexOf("<") + 1); }
-                        catch (Exception ignored){ System.out.println("Incorrect syntax at " + line); }
-
-                        try
+                        boolean b1 = true;
+                        ArrayList<Integer> r = new ArrayList<>();
+                        ArrayList<Integer> g = new ArrayList<>();
+                        ArrayList<Integer> b = new ArrayList<>();
+                        ArrayList<Integer> x = new ArrayList<>();
+                        ArrayList<Integer> y = new ArrayList<>();
+                        ArrayList<Integer> l = new ArrayList<>();
+                        ArrayList<Integer> h = new ArrayList<>();
+                        int i0 = 0;
+                        
+                        while (b1)
                         {
-                            r = Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(",")));
-                            sb.delete(0, sb.indexOf(",") + 1);
+                            try
+                            {
+                                r.add(Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))));
+                                sb.delete(0, sb.indexOf(",") + 1);
+                            }
+                            catch (Exception ignored) { System.out.println("R error at " + line); }
+
+                            try
+                            {
+                                g.add(Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))));
+                                sb.delete(0, sb.indexOf(",") + 1);
+                            }
+                            catch (Exception ignored) { System.out.println("G error at " + line); }
+
+                            try
+                            {
+                                b.add(Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))));
+                                sb.delete(0, sb.indexOf(",") + 1);
+                            }
+                            catch (Exception ignored) { System.out.println("B error at " + line); }
+
+                            try
+                            {
+                                x.add(Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))));
+                                sb.delete(0, sb.indexOf(",") + 1);
+                            }
+                            catch (Exception ignored) { System.out.println("X error at " + line); }
+
+                            try
+                            {
+                                y.add(Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))));
+                                sb.delete(0, sb.indexOf(",") + 1);
+                            }
+                            catch (Exception ignored) { System.out.println("Y error at " + line); }
+
+                            try
+                            {
+                                l.add(Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))));
+                                sb.delete(0, sb.indexOf(",") + 1);
+                            }
+                            catch (Exception ignored) { System.out.println("L error at " + line); }
+
+                            try
+                            {
+                                h.add(Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(">"))));
+                                sb.delete(0, sb.indexOf(">") + 1);
+                            }
+                            catch (Exception ignored) { System.out.println("H error at " + line); }
+                            
+                            s.setTexture(i0, r.toArray(), g.toArray(), b.toArray(), x.toArray(), y.toArray(), l.toArray(), h.toArray());
+                            i0++;
+                                
+                            if (!sb.indexOf(0).equals(","))
+                                b1 = false;
+
                         }
-                        catch (Exception ignored) { System.out.println("R error at " + line); }
-
-                        try
-                        {
-                            g = Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(",")));
-                            sb.delete(0, sb.indexOf(",") + 1);
-                        }
-                        catch (Exception ignored) { System.out.println("G error at " + line); }
-
-                        try
-                        {
-                            b = Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(",")));
-                            sb.delete(0, sb.indexOf(",") + 1);
-                        }
-                        catch (Exception ignored) { System.out.println("B error at " + line); }
-
-                        try
-                        {
-                            x = Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(",")));
-                            sb.delete(0, sb.indexOf(",") + 1);
-                        }
-                        catch (Exception ignored) { System.out.println("X error at " + line); }
-
-                        try
-                        {
-                            y = Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(",")));
-                            sb.delete(0, sb.indexOf(",") + 1);
-                        }
-                        catch (Exception ignored) { System.out.println("Y error at " + line); }
-
-                        try
-                        {
-                            l = Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(",")));
-                            sb.delete(0, sb.indexOf(",") + 1);
-                        }
-                        catch (Exception ignored) { System.out.println("L error at " + line); }
-
-                        try
-                        {
-                            h = Integer.parseInt(this.get(new StringBuilder(sb.toString()), sb.indexOf(">")));
-                            sb.delete(0, sb.indexOf(">") + 1);
-                        }
-                        catch (Exception ignored) { System.out.println("H error at " + line); }
-
-                        sb.delete(0, sb.indexOf(")") + 1);
-                        s.setTexture(r, b, g, x, y, l, h);
-
-                        if (!this.get(new StringBuilder(sb.toString()), 1).equals(","))
+                        
+                        if (sb.indexOf(1).equals("]"))
                             b0 = false;
                     }
                 }
