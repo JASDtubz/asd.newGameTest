373a4d675229e655bf9480e9ba2b3489fd7a9bdb
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-10 10:26:18 -0500

    minor preferences

--- mapCumSheet/Sperm.java
+++ mapCumSheet/Sperm.java
@@ -14,20 +14,20 @@ public class Sperm
 
     public void setTexture(int i, short[] r, short[] g, short[] b, short[] x, short[] y, short[] l, short[] h)
     {
-        Texture[] text = new Texture[i];
+        Texture[] t = new Texture[i];
 
         for (byte i_ = 0; i_ < i; i_++)
-            text[i_] = new Texture(r[i_], g[i_], b[i_], x[i_], y[i_], l[i_], h[i_]);
+            t[i_] = new Texture(r[i_], g[i_], b[i_], x[i_], y[i_], l[i_], h[i_]);
 
-        this.texture.add(text);
+        this.texture.add(t);
     }
 }
 
 class Texture
 {
-    public int r, b, g, x, y, l, h;
+    public short r, b, g, x, y, l, h;
 
-    public Texture(int r, int b, int g, int x, int y, int l, int h)
+    public Texture(short r, short b, short g, short x, short y, short l, short h)
     {
         this.r = r;
         this.b = b;
