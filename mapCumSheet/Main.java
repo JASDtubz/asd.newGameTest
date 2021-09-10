8102d9b7d7e5ad8678c9efdfeafab5d6e9f50357
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-09 21:25:46 -0500

    yup

--- mapCumSheet/Main.java
+++ mapCumSheet/Main.java
@@ -8,6 +8,9 @@ public class Main
         HashMap<Integer, Sperm> hm = l.execute();
         System.out.println(hm.get(0).name);
         System.out.println(hm.get(0).collision);
-
+        System.out.println(hm.get(0).texture.get(0)[0].r);
+        System.out.println(hm.get(0).texture.get(0)[1].r);
+        System.out.println(hm.get(0).texture.get(1)[0].r);
+        System.out.println(hm.get(1).texture.get(0)[0].r);
     }
 }
