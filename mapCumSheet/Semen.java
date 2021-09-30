a07c33b0cf5852d90a061fcea5347ee7dfbff4a1
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-30 13:33:43 -0500

    Update Semen.java

--- mapCumSheet/Semen.java
+++ mapCumSheet/Semen.java
@@ -3,11 +3,20 @@ import java.util.HashMap;
 public class Semen
 {
     public String name;
+    public int xMin, yMin, xMax, yMax;
     private HashMap<Integer, Sperm> hm;
 
     public Semen(HashMap<Integer, Sperm> hm) { this.hm = hm; }
 
     public void setName(String s) { this.name = s; }
+    
+    public void setXMin(String s) { this.xMin = Integer.parseInt(s); }
+    
+    public void setYMin(String s) { this.yMin = Integer.parseInt(s); }
+    
+    public void setXMax(String s) { this.xMax = Integer.parseInt(s); }
+    
+    public void setYMax(String s) { this.yMax = Integer.parseInt(s); }
 }
 
 class Thing
