bfa43874317f7b33f2c9e4b1ecf15dc64889763f
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-10-01 12:39:03 -0500

    easy

--- mapCumSheet/Sperm.java
+++ mapCumSheet/Sperm.java
@@ -12,11 +12,11 @@ public class Sperm
 
     public void setCollision(boolean b) { this.collision = b; }
 
-    public void setTexture(int i, short[] r, short[] g, short[] b, short[] x, short[] y, short[] l, short[] h)
+    public void setTexture(short[] r, short[] g, short[] b, short[] x, short[] y, short[] l, short[] h)
     {
         Texture[] t = new Texture[i];
 
-        for (byte i_ = 0; i_ < i; i_++) { t[i_] = new Texture(r[i_], g[i_], b[i_], x[i_], y[i_], l[i_], h[i_]); }
+        for (byte i = 0; i < r.length; i++) { t[i] = new Texture(r[i], g[i], b[i], x[i], y[i], l[i], h[i]); }
 
         this.texture.add(t);
     }
