401dbf00f9fc3169bf51e9d95cb5ded681db6bb1
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-10-05 22:56:31 -0500

    Update Tissue.java

--- mapCumSheet/Tissue.java
+++ mapCumSheet/Tissue.java
@@ -1,10 +1,13 @@
 import java.util.*;
 
-public final class Tissue
+import static java.lang.Integer.parseInt;
+import static java.lang.System.err;
+
+public class Tissue
 {
     public Tissue() { }
 
-    public final HashMap<String, Semen> execute(HashMap<Integer, Sperm> sperm)
+    public HashMap<String, Semen> execute(HashMap<Integer, Sperm> sperm)
     {
         Scanner scan = new Scanner(Objects.requireNonNull(Tissue.class.getResourceAsStream("testicle/condom.came")));
         HashMap<String, Semen> hm = new HashMap<>();
@@ -35,117 +38,153 @@ public final class Tissue
                     sb.delete(0, sb.indexOf(":") + 1);
                     b = true;
                 }
-                catch (Exception ignored) { System.err.println("No name found at " + line); }
+                catch (Exception ignored) { err.println("No name found at " + line); }
 
                 if (b)
                 {
                     try
                     {
-                        s.setName(this.get(new StringBuilder(sb.toString()), sb.indexOf(",")));
+                        s.setName(sb.substring(0, sb.indexOf(",")));
                         string = s.name;
                     }
-                    catch (Exception ignored) { System.err.println("No comma found at " + line); }
-                    
-                    b = false;
+                    catch (Exception ignored) { err.println("No comma found at " + line); }
+                    finally { b = false; }
                 }
-                
+
                 try
                 {
                     sb.delete(0, sb.indexOf(":") + 1);
                     b = true;
                 }
-                catch (Exception ignored) { System.err.println("No minimum x found at " + line); }
-                
+                catch (Exception ignored) { err.println("No minimum x found at " + line); }
+
                 if (b)
                 {
-                    try { s.setXMin(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))); }
-                    catch (Exception ignored) { System.out.println("Getting minimum x error found at " + line); }
-                    
-                    b = false;
+                    try { s.setXMin(parseInt(sb.substring(0, sb.indexOf(",")))); }
+                    catch (Exception ignored) { err.println("No minimum x value found at " + line); }
+                    finally { b = false; }
                 }
-                
+
                 try
                 {
                     sb.delete(0, sb.indexOf(":") + 1);
                     b = true;
                 }
-                catch (Exception ignored) { System.err.println("No minimum y found at " + line); }
-                
+                catch (Exception ignored) { err.println("No minimum y found at " + line); }
+
                 if (b)
                 {
-                    try { s.setYMin(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))); }
-                    catch (Exception ignored) { System.err.println("No minimum y value found at " + line); }
-                    
-                    b = false;
+                    try { s.setYMin(parseInt(sb.substring(0, sb.indexOf(",")))); }
+                    catch (Exception ignored) { err.println("No minimum y value found at " + line); }
+                    finally { b = false; }
                 }
-                
+
                 try
                 {
                     sb.delete(0, sb.indexOf(":") + 1);
                     b = true;
                 }
-                catch (Exception ignored) { System.err.println("No maximum x found at " + line); }
-                
+                catch (Exception ignored) { err.println("No maximum x found at " + line); }
+
                 if (b)
                 {
-                    try { s.setXMax(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))); }
-                    catch (Exception ignored) { System.err.println("No maximum x value found at " + line); }
-                    
-                    b = false;
+                    try { s.setXMax(parseInt(sb.substring(0, sb.indexOf(",")))); }
+                    catch (Exception ignored) { err.println("No maximum x value found at " + line); }
+                    finally { b = false; }
                 }
-                
+
                 try
                 {
                     sb.delete(0, sb.indexOf(":") + 1);
                     b = true;
                 }
-                catch (Exception ignored) { System.err.println("No maximum y found at " + line); }
-                
+                catch (Exception ignored) { err.println("No maximum y found at " + line); }
+
                 if (b)
                 {
-                    try { s.setYMax(this.get(new StringBuilder(sb.toString()), sb.indexOf(","))); }
-                    catch (Exception ignored) { System.err.println("No maximum y value found at " + line); }
-                    
-                    b = false;
+                    try { s.setYMax(parseInt(sb.substring(0, sb.indexOf(",")))); }
+                    catch (Exception ignored) { err.println("No maximum y value found at " + line); }
+                    finally { b = false; }
                 }
-                
+
                 try
                 {
-                    sb.delete(0, sb.indexOf("[") + 1);
+                    sb.delete(0, sb.indexOf(":") + 1);
                     b = true;
                 }
-                catch (Exception ignored) { System.err.println("No sperm found at " + line); }
-                
+                catch (Exception ignored) { err.println("No sperm found at " + line); }
+
                 if (b)
                 {
                     boolean b0 = true;
-                    
+                    ArrayList<Cell> al = new ArrayList<>();
+
                     while (b0)
                     {
                         boolean b1 = false;
-                        
+                        Cell c = new Cell();
+
                         try
                         {
-                            sb.delete(0, sb.indexOf("(") + 1);
+                            sb.delete(0, sb.indexOf(":") + 1);
                             b1 = true;
                         }
-                        catch (Exception ignored) { System.err.println("No cell found at " + line); }
+                        catch (Exception ignored) { err.println("No x found at " + line); }
+
+                        if (b1)
+                        {
+                            try { c.setX(parseInt(sb.substring(0, sb.indexOf(",")))); }
+                            catch (Exception ignored) { err.println("No x value found at " + line); }
+                            finally { b1 = false; }
+                        }
+
+                        try
+                        {
+                            sb.delete(0, sb.indexOf(":") + 1);
+                            b1 = true;
+                        }
+                        catch (Exception ignored) { err.println("No y found at " + line); }
+
+                        if (b1)
+                        {
+                            try { c.setY(parseInt(sb.substring(0, sb.indexOf(",")))); }
+                            catch (Exception ignored) { err.println("No y value found at " + line); }
+                        }
                         
-                        //
-                        
-                        if (sb.indexOf("]") == 1) { b0 = false; }
+                        b1 = true;
+
+                        while (b1)
+                        {
+                            HashMap<String, String[]> hms = new HashMap<>();
+                            String str0 = "";
+                            boolean b2 = false;
+
+                            try
+                            {
+                                sb.delete(0, sb.indexOf(":") + 1);
+                                b2 = true;
+                            }
+                            catch (Exception ignored) { err.println("No id found at " + line); }
+                            
+                            if (b2)
+                            {
+                                try { str0 = sb.substring(0, sb.indexOf("<")); }
+                                catch (Exception ignored) { err.println("No id value found at " + line); }
+                            }
+
+                            while (b2)
+                            {
+                                ArrayList<String> als = new ArrayList<>();
+
+
+                            }
+
+                        }
                     }
                 }
             }
         }
 
         return hm;
-    }
-
-    private final String get(StringBuilder sb, int i)
-    {
-        sb.delete(i, sb.length());
-
-        return sb.toString();
     }
 }
