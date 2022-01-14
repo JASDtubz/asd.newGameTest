fa60b2be738585b953f08b70c42343cbf4563eda
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-14 06:51:37 -0600

    Update Cell.java

--- mapCumSheet/Cell.java
+++ mapCumSheet/Cell.java
@@ -1,1 +1,15 @@
+import java.util.HashMap;
 
+public class Cell
+{
+    public int x, y;
+    public HashMap<String, String> hm;
+
+    public Cell() { }
+
+    public void setX(int i) { this.x = i; }
+
+    public void setY(int i) { this.y = i; }
+
+    public void setText(HashMap<String, String> hm) { this.hm = hm; }
+}
