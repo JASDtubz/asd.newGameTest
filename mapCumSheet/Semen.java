fb07053fd8d35800fe058c2339cbdafaf678903a
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-18 08:45:38 -0600

    _LIST_

--- mapCumSheet/Semen.java
+++ mapCumSheet/Semen.java
@@ -5,6 +5,7 @@ public final class Semen
     public String name;
     public int xMin, yMin, xMax, yMax;
     public HashMap<String, Cell> hm;
+    public String[] _LIST_;
 
     public Semen() { }
 
@@ -19,4 +20,6 @@ public final class Semen
     public void setYMax(int i) { this.yMax = i; }
 
     public void addCells(HashMap<String, Cell> hm) { this.hm = hm; }
+    
+    public void list(String[] list) { this._LIST_ = list; }
 }
