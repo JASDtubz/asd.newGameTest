1042ecc0fca8ec3d94243a0a6b1e7e99afbce9bf
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-17 21:13:25 -0600

    Update Tissue.java

--- mapCumSheet/Tissue.java
+++ mapCumSheet/Tissue.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -10,7 +9,7 @@ public class Tissue
 {
     public Tissue() { }
 
-    public HashMap<String, Semen> execute()
+    synchronized public HashMap<String, Semen> execute()
     {
         Scanner scan = new Scanner(requireNonNull(Tissue.class.getResourceAsStream("testicle/condom.came")));
         HashMap<String, Semen> hm = new HashMap<>();
@@ -110,7 +109,6 @@ public class Tissue
                     finally { b = false; }
                 }
 
-                //Checks Sperm
                 try
                 {
                     sb.delete(0, sb.indexOf("[") + 1);
@@ -118,23 +116,11 @@ public class Tissue
                 }
                 catch (Exception ignored) { err.println("No sperm found at " + line); }
 
-                /*
-                        TODO create Cell here
-                    TODO add Cell
-                        create Cell Objects here
-                    TODO in Cell add Cell Objects
-                        create text here
-                    TODO in Cell Objects add text
-                    TODO return List of Cells
-                 */
-
-
                 if (b)
                 {
                     HashMap<String, Cell> hmc = new HashMap<>();
                     boolean b0 = true;
 
-                    //  Cell Loop
                     while (b0)
                     {
                         Cell c = new Cell();
@@ -169,12 +155,10 @@ public class Tissue
                         {
                             try { c.setY(parseInt(sb.substring(0, sb.indexOf(",")))); }
                             catch (Exception ignored) { err.println("No y value at " + line); }
-                            //Redundant finally statement
                         }
 
                         b1 = true;
 
-                        //  String Loop
                         while (b1)
                         {
                             String stringName = "";
@@ -194,10 +178,6 @@ public class Tissue
                                 catch (Exception ignored) { err.println("No name value at " + line); }
                                 finally { b2 = false; }
                             }
-
-                            System.out.println(sb);
-                            System.out.println(stringName);
-                            System.exit(0);
 
                             try
                             {
@@ -223,173 +203,19 @@ public class Tissue
                         c.setText(hms);
                         hmc.put(ctr, c);
 
+                        sb.delete(0, sb.indexOf(")") + 1);
 
+                        if (!sb.substring(0, 1).equals(",")) { b0 = false; }
+                        else { sb.delete(0, 1); }
                     }
 
                     s.addCells(hmc);
                 }
 
                 hm.put(string, s);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                //Gets Sperm
-                if (b)
-                {
-                    HashMap<String, String[]> hms = new HashMap<>();        //For text
-                    ArrayList<Cell> al = new ArrayList<>();                 //For Cell list
-                    boolean b0 = true;
-
-                    //Gets Sperm                                            add Cell
-                    while (b0)
-                    {
-                        boolean b1 = false;
-                        Cell c = new Cell();
-
-                        //Checks x
-                        try
-                        {
-                            sb.delete(0, sb.indexOf(":") + 1);
-                            b1 = true;
-                        }
-                        catch (Exception ignored) { err.println("No x found at " + line); }
-
-                        //Gets x
-                        if (b1)
-                        {
-                            try { c.setX(parseInt(sb.substring(0, sb.indexOf(",")))); }
-                            catch (Exception ignored) { err.println("No x value found at " + line); }
-                            finally { b1 = false; }
-                        }
-
-                        //Checks y
-                        try
-                        {
-                            sb.delete(0, sb.indexOf(":") + 1);
-                            b1 = true;
-                        }
-                        catch (Exception ignored) { err.println("No y found at " + line); }
-
-                        //Gets y
-                        if (b1)
-                        {
-                            try { c.setY(parseInt(sb.substring(0, sb.indexOf(",")))); }
-                            catch (Exception ignored) { err.println("No y value found at " + line); }
-                        }
-
-                        b1 = true;
-
-                        //Checks id box                                     Cell Objects
-                        while (b1)
-                        {
-                            ArrayList<String> als = new ArrayList<>();
-                            String str0 = "";
-                            boolean b2 = false;
-
-                            //Checks id
-                            try
-                            {
-                                sb.delete(0, sb.indexOf(":") + 1);
-                                b2 = true;
-                            }
-                            catch (Exception ignored) { err.println("No id found at " + line); }
-
-                            //Gets id
-                            if (b2)
-                            {
-                                try { str0 = sb.substring(0, sb.indexOf("<")); }
-                                catch (Exception ignored) { err.println("No id value found at " + line); }
-                            }
-
-                            //Checks strings                                text
-                            while (b2)
-                            {
-                                boolean b3 = false;
-
-                                //Checks string
-                                try
-                                {
-                                    sb.delete(0, sb.indexOf("<") + 1);
-                                    b3 = true;
-                                }
-                                catch (Exception ignored)
-                                {
-                                    try
-                                    {
-                                        sb.delete(0, sb.indexOf(",") + 1);
-                                        b3 = true;
-                                    }
-                                    catch (Exception ignored0) { err.println("No string found at " + line); }
-                                }
-
-                                //Gets string
-                                if (b3)
-                                {
-                                    try { als.add(this.fixString(new StringBuilder(sb.substring(0, sb.indexOf("$b"))))); }
-                                    catch (Exception ignored) { err.println("No string value found at " + line); }
-                                    finally { sb.delete(0, sb.indexOf("$b") + 2); }
-                                }
-
-                                //Checks comma
-                                if (sb.indexOf(">") == 0) { b2 = false; }
-                            }
-
-                            hms.put(str0, this.getList(als));
-
-                            //Checks comma too
-                            if (sb.indexOf("]") == 1) { b1 = false; }
-                        }
-
-                        //c.setText(hms);
-                        al.add(c);
-                    }
-
-                    //s.addCell();
-                }
             }
         }
 
         return hm;
-    }
-
-    public String[] getList(ArrayList<String> al)
-    {
-        String[] ss = new String[al.size()];
-
-        for (int i = 0; i < al.size(); i++) { ss[i] = al.get(i); }
-
-        return ss;
-    }
-
-    public String fixString(StringBuilder sb)
-    {
-        boolean b = true;
-
-        while (b)
-        {
-            try { sb.replace(sb.indexOf("_"), sb.indexOf("_"), " "); }
-            catch (Exception ignored) { b = false; }
-        }
-
-        while (!b)
-        {
-            try { sb.replace(sb.indexOf("`"), sb.indexOf("`"), ","); }
-            catch (Exception ignored) { b = true; }
-        }
-
-        return sb.toString();
     }
 }
