bc305587d7c25e23743a2069c0b92a05c24e2027
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-18 07:48:44 -0600

    updated hashmap to string

--- mapCumSheet/Main.java
+++ mapCumSheet/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         Lotion l = new Lotion();
-        HashMap<Integer, Sperm> hm = l.execute();
+        HashMap<String, Sperm> hm = l.execute();
         Tissue t = new Tissue();
         HashMap<String, Semen> hm_ = t.execute();
 
