cd03a3affe0346c9fea8d637de0dca6c74c6db30
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-26 11:18:20 -0600

    Update cumapi.java

--- cumapi.java
+++ cumapi.java
@@ -1,47 +1,49 @@
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application
 {
-    static ChoiceBox<String> cb = new ChoiceBox<>();
-    static BorderPane bp = new BorderPane();
-    static Scene scene;
+    ChoiceBox<String> cb = new ChoiceBox();
+    BorderPane bp = new BorderPane();
+    Scene scene;
 
-    public static void main(String[] q) { Application.launch(); }
+    public Main() { }
+    
+    public static void main(String[] q) { Application.launch(q); }
 
     @Override
     public void start(Stage s)
     {
         Label l = new Label("loading...");
         Scene temp = new Scene(l);
+        
         s.setScene(temp);
         s.show();
 
-        try { System.class.wait(1000); }
-        catch (InterruptedException ignored) { }
-
         String[] str = { "one", "two" };
 
-        for (String string : str) { cb.getItems().add(string); }
+        for (String string : str) { this.cb.getItems().add(string); }
 
-        cb.getItems().add("three");
-        cb.setOnAction(e -> event());
-        cb.setValue(str[0]);
+        this.cb.getItems().add("three");
+        this.cb.setValue(str[0]);
+        
+        Button object = new Button("Object");
+        Button map = new Button("Map");
+        
+        ToolBar tb = new ToolBar(object, map);
 
-        bp.setRight(cb);
-        scene = new Scene(bp, 854, 480);
-
+        this.bp.setTop(tb);
+        this.bp.setRight(cb);
+        this.scene = new Scene(bp, 854, 480);
+        
         s.hide();
-        s.setScene(scene);
+        s.setScene(this.scene);
         s.show();
-    }
-
-    public void event()
-    {
-        System.out.println(cb.getValue());
     }
 }
